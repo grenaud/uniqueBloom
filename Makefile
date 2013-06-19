96dff0f2922248cdec75cc727d95b3951cfc2186
grenaud <gabriel.reno@gmail.com>
2013-06-19 15:30:52 +0200

    Added support for two reads

--- Makefile
+++ Makefile
@@ -2,26 +2,26 @@
 CXX      = g++   
 LIBGAB   = /home/gabriel_renaud/lib/
 
-CXXFLAGS = -Wall -lm -O3 -lz -I${LIBGAB} -I${LIBGAB}/VCFparser/  -c
+CXXFLAGS = -Wall -lm -O3 -lz -I${LIBGAB} -I${LIBGAB}/VCFparser/ -I${LIBGAB}/VCFparser/gzstream/  -c
 LDFLAGS  = -lz
 
 
-all: uniqueseq  uniqueseqBloom
+all:   uniqueseqBloom #uniqueseq
 
-uniqueseq.o:	uniqueseq.cpp
-	${CXX} ${CXXFLAGS} uniqueseq.cpp
+# uniqueseq.o:	uniqueseq.cpp
+# 	${CXX} ${CXXFLAGS} uniqueseq.cpp
 
-uniqueseq:	uniqueseq.o ${LIBGAB}utils.o   ${LIBGAB}/VCFparser/FastQParser.o ${LIBGAB}/VCFparser/FastQObj.o  PrefixTree.o
-	${CXX} $(LDFLAGS) -o $@ $^ $(LDLIBS)
+# uniqueseq:	uniqueseq.o ${LIBGAB}utils.o   ${LIBGAB}/VCFparser/FastQParser.o ${LIBGAB}/VCFparser/FastQObj.o  ${LIBGAB}/VCFparser/gzstream/libgzstream.a PrefixTree.o
+# 	${CXX}  -o $@ $^ $(LDLIBS) $(LDFLAGS)
 
 uniqueseqBloom.o:	uniqueseqBloom.cpp
 	${CXX} ${CXXFLAGS} -I/home/gabriel_renaud/scripts/uniqueSeqs/dablooms/dablooms/src/ uniqueseqBloom.cpp
 
-uniqueseqBloom:	uniqueseqBloom.o ${LIBGAB}utils.o   ${LIBGAB}/VCFparser/FastQParser.o ${LIBGAB}/VCFparser/FastQObj.o /home/gabriel_renaud/scripts/uniqueSeqs/dablooms/dablooms/build/libdablooms.a
-	${CXX} $(LDFLAGS) -o $@ $^ $(LDLIBS)
+uniqueseqBloom:	uniqueseqBloom.o ${LIBGAB}utils.o  ${LIBGAB}/VCFparser/FastQParser.o ${LIBGAB}/VCFparser/FastQObj.o /home/gabriel_renaud/scripts/uniqueSeqs/dablooms/dablooms/build/libdablooms.a  ${LIBGAB}/VCFparser/gzstream/libgzstream.a 
+	${CXX}  -o $@ $^ $(LDLIBS) $(LDFLAGS)
 
-PrefixTree.o:	PrefixTree.cpp
-	g++ -g $(CXXFLAGS) -c  $^
+# PrefixTree.o:	PrefixTree.cpp
+# 	g++ -g $(CXXFLAGS) -c  $^
 
 clean :
 	rm -f uniqueseq.o uniqueseq PrefixTree.o uniqueseqBloom
--- uniqueseqBloom.cpp
+++ uniqueseqBloom.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <gzstream.h>
 
 #include "FastQParser.h"
 #include "FastQObj.h"
@@ -23,53 +24,131 @@ extern "C" {
 using namespace std;
 
 int main (int argc, char *argv[]) {
+    bool isFasta   =false;
+    bool hasReverse=false;
+    //    bool outputSpecified=false;
+
+    string revReads;
+    string outRevReads;
+    string outputStr="/dev/stdout";
+
+    ogzstream myFileo;
+    ogzstream myFileo2;
+
+    // streambuf  * buf;    
 
     if(argc ==1 ){
-	cerr<<"Usage uniqueseqBloom [fastq file] [temp file]"<<endl;
+	cerr<<"Usage uniqueseqBloom [fastq file] [temp file] "<<endl;
 	cerr<<"the [temp file] can be whatever you wish, just pick a file you can write to"<<endl;
+	cerr<<"Options:"<<endl;
+	cerr<<"\t-f\tInput is fasta"<<endl;
+	cerr<<"\t-o\tOutput (default: stdout)"<<endl;
+	cerr<<"\t-r\tInput Reverse reads"<<endl;
+	cerr<<"\t-p\tOutput Reverse reads"<<endl;
+
 	return 1;
     }
+
+    for(int i=1;i<(argc-2);i++){ 
+
+	if(string(argv[i]) == "-f"){
+            isFasta=true;
+            continue;
+        }
+
+	if(string(argv[i]) == "-r"){
+            hasReverse=true;
+	    revReads=string(argv[i+1]);
+	    i++;
+            continue;
+        }
+
+	if(string(argv[i]) == "-p"){
+	    outRevReads=string(argv[i+1]);
+	    i++;
+            continue;
+        }
+
+	if(string(argv[i]) == "-o"){
+	    outputStr=string(argv[i+1]);
+	    // buf =  new     streambuf (outputStr);
+	    //	    outputSpecified=true;
+	    i++;
+            continue;
+        }
+
+    }
+
+
+
+    
+
+    if(outRevReads.empty()){
+
+	if(hasReverse){
+	    cerr<<"You must specify the output for the reverse reads"<<endl;
+	    return 1;
+	}
+
+    }
+
+    // if(!outputSpecified){
+    // 	buf  =  cout.rdbuf();
+    // }
+
+    //    myFileo(buf);
+    myFileo.open(outputStr.c_str(), ios::out);
+    myFileo2.open(outRevReads.c_str(), ios::out);
 
     counting_bloom_t *bloom;
 
     // PrefixTree<int>  trieSeq;
-    FastQParser fqp (argv[1]);
+    FastQParser fqp (argv[argc-2],isFasta);
+    FastQParser * fqp2=0;
 
-    if (!(bloom = new_counting_bloom(CAPACITY, ERROR_RATE, argv[2]))) {
+    if(hasReverse){
+	fqp2  = new FastQParser(revReads,isFasta);
+    }
+    
+    if (!(bloom = new_counting_bloom(CAPACITY, ERROR_RATE, argv[argc-1]))) {
         cerr<<"ERROR: Could not create bloom filter\n"<<endl;
         return EXIT_FAILURE;
     }else{
-	cerr<<"Created bloom filter "<<argv[1]<<endl;
+	cerr<<"Created bloom filter "<<argv[argc-1]<<endl;
     }
 
     while(fqp.hasData()){
 
 	// cout<<"l1"<<endl;
     	FastQObj * test	=fqp.getData();
-    
+    	FastQObj * test2=0;
+	    //=fqp.getData();
+	
+	string * strtocheck=test->getSeq();
 
-	if( counting_bloom_check(bloom, test->getSeq()->c_str(), test->getSeq()->size()  )== 0 ){ //not found
-	    cout<<*test<<endl;
-	    counting_bloom_add(bloom,  test->getSeq()->c_str(), test->getSeq()->size() );
-	}else{
+	if(hasReverse){
+	    fqp2->hasData();	    
+	    test2=fqp2->getData();
+	    //strtocheck=strtocheck+"#"+test->getSeq();
+	    strtocheck->append(string("#")+*(test2->getSeq()) );
+	    //	    cout<<*strtocheck<<endl;
+	}
+
+	if( counting_bloom_check(bloom, strtocheck->c_str(), strtocheck->size()  )== 0 ){ //not found
+	    myFileo <<*test<<endl;
+	    if(hasReverse){ 	    myFileo2<<*test2<<endl; }
+	    counting_bloom_add(bloom,  strtocheck->c_str(), strtocheck->size() );
+	}else{//not unique
 	    //do nothing
 	}
 
 
-	// cout<<"l2"<<endl;	
-	//trieSeq.searchMismatch( (test->getSeq())->c_str(),&matches,0);
-	// cout<<matches.size()<<endl;
-	// if(matches.empty()){//not found
-	//     trieSeq.insertIntoTree( test->getSeq()->c_str()  , 0);
-	//     cout<<*test<<endl;
-	// }else{
-	//     //skip
-	//     //cout<<"skip "<<*test<<endl;
-
-	// }
 	    
     }
     free_counting_bloom(bloom);
+
+    myFileo.close();
+    myFileo2.close();
 
     return 0;
 }
